ca2b080fdfa5a2cc23d371d43ffedf09ab80e490
Saul H <hl.saul.01@gmail.com>
2022-11-09 23:13:11 -0600

    🎉Initial commit

---
+++ src/Bootstrap.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types = 1);
+
+use Symfony\Component\Dotenv\Dotenv;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/../.env');
